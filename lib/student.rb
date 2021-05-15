e496b2b8bb46d83d66644d7e5d2ce131b507d900
Lena Hou <lenahou2013@gmail.com>
2021-05-15 19:39:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,9 +31,10 @@ end
 
 def save
   sql = <<-SQL
-  INSERT INTO students (name, album)
+  INSERT INTO students (name, grade)
   VALUES (?, ?)
   SQL
+  DB[:conn].execute(sql, self.name, self.grade)
 end
 
 def self.create
