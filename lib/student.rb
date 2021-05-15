d662a78f3d2f6ed61c9361353f6bf4766a77392f
Lena Hou <lenahou2013@gmail.com>
2021-05-15 19:31:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,11 @@
 class Student
-  attr_accessor :id
+  attr_accessor :id, :name, :grade
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-def initialize (id = nil)
+def initialize (name, grade, id = nil)
+  @name = name
+  @grade = grade
   @id = id
 end
 
